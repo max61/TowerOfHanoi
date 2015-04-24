f82e56c077fac39631bff67732821225d6b89ce3
Max Chiu <maxchiu@apples-iMac.local>
2015-04-24 15:42:54 +0800

    Minor Changes

---
+++ Tower of Hanoi (GUI)/GraphicView.swift
@@ -0,0 +1,19 @@
+//
+//  GraphicView.swift
+//  Tower of Hanoi (GUI)
+//
+//  Created by Max Chiu on 24/4/15.
+//  Copyright (c) 2015 C&A Computer Consultants Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class GraphicView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
--- Tower of Hanoi (GUI)/MainWindowController.swift
+++ Tower of Hanoi (GUI)/MainWindowController.swift
@@ -10,6 +10,8 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
 
+    
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
