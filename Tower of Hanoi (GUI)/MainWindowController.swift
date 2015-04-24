f728fc14234826aa498cdf8a1f70ee635c46eb3e
Max Chiu <maxchiu@apples-iMac.local>
2015-04-24 15:34:39 +0800

    MainWindowController Class (and xib) Add

---
+++ Tower of Hanoi (GUI)/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  Tower of Hanoi (GUI)
+//
+//  Created by Max Chiu on 24/4/15.
+//  Copyright (c) 2015 C&A Computer Consultants Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
