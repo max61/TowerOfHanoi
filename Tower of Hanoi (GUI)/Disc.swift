255c97b0fa5fc413ee94b35bf6575cebab0b4bc9
Max Chiu <max@caconsultant.com>
2015-04-24 20:44:11 +0800

    Add Class Disc

---
+++ Tower of Hanoi (GUI)/Disc.swift
@@ -0,0 +1,13 @@
+//
+//  Disc.swift
+//  Tower of Hanoi (GUI)
+//
+//  Created by Max Chiu on 24/4/15.
+//  Copyright (c) 2015 C&A Computer Consultants Ltd. All rights reserved.
+//
+
+import Foundation
+
+class Disc {
+    
+}
