8933a4fced197c71a4cc3176f5ebdb03f7082f6b
Max Chiu <maxchiu@apples-iMac.local>
2015-04-24 15:21:36 +0800

    Initial Commit

---
+++ Tower of Hanoi (GUI)/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Tower of Hanoi (GUI)
+//
+//  Created by Max Chiu on 24/4/15.
+//  Copyright (c) 2015 C&A Computer Consultants Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tower of Hanoi (GUI)Tests/Tower_of_Hanoi__GUI_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tower_of_Hanoi__GUI_Tests.swift
+//  Tower of Hanoi (GUI)Tests
+//
+//  Created by Max Chiu on 24/4/15.
+//  Copyright (c) 2015 C&A Computer Consultants Ltd. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Tower_of_Hanoi__GUI_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
